8ffe93c6a8374f545867eb83ae71382c175b0461
Adel_3d <139482690+adel-abdellaoui4d@users.noreply.github.com>
2024-03-29 09:53:58 +0100

    Add files via upload
    This is my first react project !

---
+++ src/components/accordian/data.js
@@ -0,0 +1,31 @@
+const data = [
+{
+    id : '1',
+    question : "What are accordian componenets?",
+    answer:"Accordian components are used to group and organize content into collapsable sections."
+},
+{
+    id : '2',
+    question : "What are they used for?",
+    answer:"They are commonly empoyed in various context such as FAQs",
+},
+{
+    id : '3',
+    question : "accordian as a musical instrument",
+    answer:"The accordian is a musical instrument with a keyword and bell  ",
+},
+{
+    id : '4',
+    question : "Can i create an accordian component with a different faq answer",
+    answer:"Yes of course , it is very possible to create an accordian components with it",
+}];
+
+
+export default data;
+
+
+
+
+
+
+
